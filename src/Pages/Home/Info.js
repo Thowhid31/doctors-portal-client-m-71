bd410cc5aee235b415ca0323c36b8acb57405a5a
Thowhid31 <thowhidantor16@gmail.com>
2022-05-10 09:17:34 +0600

    implement card design with dynamic form

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import InfoCards from './InfoCards';
 import clock from '../../assets/icons/clock.svg'
+import marker from '../../assets/icons/marker.svg'
+import phone from '../../assets/icons/phone.svg'
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCards img={clock}/>
-            <InfoCards img={clock}/>
-            <InfoCards img={clock}/>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+            <InfoCards cardTitle="Opening Ours" cardDetails="Our Opening Clock doesn't stops."  bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}/>
+            <InfoCards cardTitle="Our Locations" cardDetails="Dhaka, Mymensingh is our Location to locate us." bgClass="bg-accent" img={marker}/>
+            <InfoCards cardTitle="Contact Us" cardDetails="24/7 Customer service provided for you." bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}/>
         </div>
     );
 };
--- src/Pages/Home/InfoCards.js
+++ src/Pages/Home/InfoCards.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const InfoCards = ({ img }) => {
+const InfoCards = ({ img, cardTitle, cardDetails, bgClass }) => {
     return (
-        <div class="card lg:card-side bg-base-100 shadow-xl pl-6">
-            <figure><img src={img} alt="Album"></img></figure>
+        <div class={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
+            <figure className='p-6'><img src={img} alt="Album"></img></figure>
             <div class="card-body text-white">
-                <h2 class="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+                <h2 class="card-title">{cardTitle}</h2>
+                <p>{cardDetails}</p>
             </div>
         </div>
     );
