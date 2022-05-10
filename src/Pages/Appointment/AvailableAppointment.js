11880552140c3034fc6a0bccf51620c0a315d6f2
Thowhid31 <thowhidantor16@gmail.com>
2022-05-10 23:09:49 +0600

    Added card div to the appointment section

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,10 +1,20 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AvailableAppointment = ({date}) => {
+    const [services, setServices] = useState([]);
+
+    useEffect(()=>{
+        fetch('appointment.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    },[])
     return (
         <div>
-            Available Appointments: {format(date, 'PP')}
+            <h4 className='text-xl text-primary text-center'>Available Appointments: {format(date, 'PP')}</h4>
+            <div>
+                
+            </div>
         </div>
     );
 };
