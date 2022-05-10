deffd9d6ce84e3da27439aa152b7eb35872acfd7
Thowhid31 <thowhidantor16@gmail.com>
2022-05-10 09:53:46 +0600

    added the section of treatment

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <div class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} />
+                <img src={chair} alt='chair' />
                 <div>
                     <h1 class="text-left text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p class="text-left py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
