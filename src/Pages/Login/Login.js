a895205567ed3b9f9d1c985737f4a3c3cf23b74b
Thowhid31 <thowhidantor16@gmail.com>
2022-05-11 23:09:55 +0600

    added react form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    
+    const onSubmit = data => {
+        console.log(data);
+    }
 
-    if(user){
+    if (user) {
         console.log(user);
     }
     return (
@@ -13,13 +19,21 @@ const Login = () => {
             <div class="card w-96 bg-base-100 shadow-xl">
                 <div class="card-body">
                     <h2 class="text-center text-2xl font-bold">Login</h2>
-                    
 
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input {...register("firstName", { required: true })} />
+                        {errors.firstName?.type === 'required' && "First name is required"}
+
+                        <input {...register("lastName", { required: true })} />
+                        {errors.lastName && "Last name is required"}
+
+                        <input type="submit" />
+                    </form>
                     <div class="divider">OR</div>
-                    <button 
-                    onClick={()=> signInWithGoogle()}
-                    class="btn btn-outline">Continue with Google</button>
-                    
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        class="btn btn-outline">Continue with Google</button>
+
                 </div>
             </div>
         </div>
