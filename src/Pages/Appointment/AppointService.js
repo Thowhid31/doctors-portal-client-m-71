78e8791f943601d098a85c7e3a9c43b148af2ad8
Thowhid31 <thowhidantor16@gmail.com>
2022-05-11 09:42:14 +0600

    todays last commit

--- src/Pages/Appointment/AppointService.js
+++ src/Pages/Appointment/AppointService.js
@@ -4,8 +4,8 @@ const AppointService = ({ service, setTreatment }) => {
     const { name, slots } = service;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-        <div class="card-body">
-          <h2 class="card-title text-secondary">{name}</h2>
+        <div class="card-body text-center">
+          <h2 class="text-xl font-bold text-secondary">{name}</h2>
           <p>{
           slots.length ? <span>{slots[0]}</span>
           : <span className='text-red-500'>Try another date.</span>
@@ -17,7 +17,7 @@ const AppointService = ({ service, setTreatment }) => {
             <label
              disabled={slots.length===0} 
              onClick={()=>setTreatment(service)}
-             for="booking-modal" class="btn btn-secondary text-white">Book Appointment</label>
+             for="booking-modal" class="btn btn-sm btn-secondary text-white bg-gradient-to-r from-secondary to-primary">Book Appointment</label>
           </div>
         </div>
       </div>
--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -9,13 +9,13 @@ const AvailableAppointment = ({date}) => {
     const [treatment, setTreatment] = useState(null)
 
     useEffect(()=>{
-        fetch('appointment.json')
+        fetch('http://localhost:5000/service')
         .then(res => res.json())
         .then(data => setServices(data))
     },[])
     return (
         <div>
-            <h4 className='text-xl text-primary text-center'>Available Appointments: {format(date, 'PP')}</h4>
+            <h4 className='text-xl text-primary text-center my-12'>Available Appointments: {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
                     services.map(service => <AppointService
