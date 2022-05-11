52ad1dd02d552a681a3ccb9fedfecd92f17e7db3
Thowhid31 <thowhidantor16@gmail.com>
2022-05-11 08:22:52 +0600

    commit for new slotes console

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,21 +1,32 @@
 import React from 'react';
+import { format } from 'date-fns';
 
-const BookingModal = ({date, treatment}) => {
-    const {name, slots} = treatment;
+const BookingModal = ({ date, treatment }) => {
+    const { name, slots } = treatment;
+
+    const handleBooking = event => {
+        event.preventDefault();
+        const slot = event.target.slot.value;
+        console.log(slot);
+    }
     return (
         <div>
             <input type="checkbox" id="booking-modal" class="modal-toggle" />
             <div class="modal modal-bottom sm:modal-middle">
                 <div class="modal-box">
-                <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 class="font-bold text-lg text-secondary">Booking for: {name}</h3>
-                    <form className='grid grid-cols-1 gap-4 p-3 justify-center mt-3'>
-                    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                    <input type="submit" value='submit' placeholder="Type here" class="btn btn-secondary w-full max-w-xs" />
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-4 p-3 justify-items-center mt-3'>
+                        <input type="text" disabled value={format(date, 'PP')} class="input input-bordered w-full max-w-xs" />
+                        <select name='slot' class="select select-bordered w-full max-w-xs">
+                            {
+                                slots.map(slot => <option value={slot}>{slot}</option>)
+                            }
+                        </select>
+                        <input type="text" name='name' placeholder="Your Name Here" class="input input-bordered w-full max-w-xs" />
+                        <input type="email" name='email'  placeholder="Email Here" class="input input-bordered w-full max-w-xs" />
+                        <input type="number" name='phone' placeholder="Phone Number" class="input input-bordered w-full max-w-xs" />
+                        <input type="submit" value='submit' placeholder="Type here" class="btn btn-secondary w-full max-w-xs" />
                     </form>
                 </div>
             </div>
