9deae7dd22815884aa5906b8531d7a8d38aab8fa
Thowhid31 <thowhidantor16@gmail.com>
2022-05-12 08:58:21 +0600

    create new user for sign up

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
 
@@ -16,19 +16,26 @@ const SignUp = () => {
         error,
       ] = useCreateUserWithEmailAndPassword(auth);
 
+      const navigate = useNavigate();
+
+      const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
       let signInErrorMessage;
 
-      if(loading || gLoading){
+      if(loading || gLoading || updating){
           return <Loading/>
       }
 
-      if(error || gError){
-        signInErrorMessage = <p className='text-red-600'>{error?.message || gError?.message}</p>
+      if(error || gError || updateError){
+        signInErrorMessage = <p className='text-red-600'>{error?.message || gError?.message || updateError?.message}</p>
       }
 
-    const onSubmit = data => {
+    const onSubmit = async data => {
         console.log(data);
-        createUserWithEmailAndPassword(data.email, data.password);
+        await createUserWithEmailAndPassword(data.email, data.password);
+        await updateProfile ({displayName: data.name});
+        console.log('update done');
+        navigate('/appointment');
     }
 
     if (gUser || user) {
