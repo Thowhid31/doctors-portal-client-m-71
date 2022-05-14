5af31e29c79769059c5a3f111dbf0b60c84f77fe
Thowhid31 <thowhidantor16@gmail.com>
2022-05-14 09:42:02 +0600

    implement react query with reloading and refetch

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -4,7 +4,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import {toast } from 'react-toastify';
 
-const BookingModal = ({ date, treatment, setTreatment }) => {
+const BookingModal = ({ date, treatment, setTreatment, refetch }) => {
     const {_id, name, slots } = treatment;
     const [user] = useAuthState(auth);
     const formattedDate = format(date, 'PP');
@@ -40,6 +40,7 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
             else{
                 toast.error(`You already have an appointment on — ${data.booking?.date} at ${data.booking?.slot}`)
             }
+            refetch()
             //to close modal
         setTreatment(null);
         })
