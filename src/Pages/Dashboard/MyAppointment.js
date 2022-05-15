7e79e6e2b185a6e3826336853a8c0a33160a2131
Thowhid31 <thowhidantor16@gmail.com>
2022-05-15 09:37:51 +0600

    implement jwt for my appointment dashboard

--- src/Pages/Dashboard/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment.js
@@ -16,8 +16,17 @@ const MyAppointment = () => {
                 'authorization' : `Bearer ${localStorage.getItem('accessToken')}`
               }
             })
-            .then(res => res.json())
-            .then(data => setAppointments(data))
+            .then(res => {
+              if(res.status === 401 || res.status === 403){
+                
+              }
+  
+              console.log('res', res);
+             return  res.json()
+            })
+            .then(data => {
+              setAppointments(data);
+            })
         }
 
     }, [user])
