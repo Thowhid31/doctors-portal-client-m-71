4dc594dc4639ba47f3056f26cb8db8ac8747b8d1
Thowhid31 <thowhidantor16@gmail.com>
2022-05-17 20:09:32 +0600

    added manage doctor

--- src/Pages/Dashboard/ManageDoc.js
+++ src/Pages/Dashboard/ManageDoc.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import DocRow from './DocRow';
 
 const ManageDoc = () => {
-    const {data: doctors, isLoading} = useQuery('doctors', ()=> fetch('http://localhost:5000/doctor', {
+    const {data: doctors, isLoading, refetch} = useQuery('doctors', ()=> fetch('http://localhost:5000/doctor', {
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
@@ -16,6 +17,31 @@ const ManageDoc = () => {
     return (
         <div>
             <h1 className="2xl">Manage Doc: {doctors.length}</h1>
+            <div class="overflow-x-auto">
+  <table class="table w-full">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Avatar</th>
+        <th>Name</th>
+        <th>Specialty</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      
+      {
+          doctors.map((doctor, index) => <DocRow
+          key={doctor._key}
+          doctor={doctor}
+          index={index}
+          refetch={refetch}
+          ></DocRow>)
+      }
+      
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
