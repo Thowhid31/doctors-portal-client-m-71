5cd692af64b4bf9ee5e6c1b0ccd2d4264c8b92b8
Thowhid31 <thowhidantor16@gmail.com>
2022-05-18 07:52:34 +0600

    Added payment method with stripe

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,7 +1,12 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L0Us5A4HPuCHW2LKOm7foxYCz3Wqe3PJCM5YrIS6qIWs2ZjDQAE2wAJrnpqp8lJD09wft5X5JBnRDESSKh9Ib8w00usanoxlr');
 
 const Payment = () => {
     const { id } = useParams();
@@ -20,19 +25,25 @@ const Payment = () => {
 
     return (
         <div>
-                    <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
-                        <div class="card-body">
-                            <p className='text-success font-bold'>Hello, {appointment.patientName}</p>
-                            <h2 class="card-title"> Pay for {appointment.treatment}</h2>
-                            <p>Your Appointment <span className="text-primary">{appointment.date}</span> at {appointment.slot}</p>
-                            <p>Please Pay: ${appointment.price}</p>
-                        </div>
-                    </div>
-                    <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
-                        <div class="card-body">
-                        </div>
-                    </div>
+            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div class="card-body">
+                    <p className='text-success font-bold'>Hello, {appointment.patientName}</p>
+                    <h2 class="card-title"> Pay for {appointment.treatment}</h2>
+                    <p>Your Appointment <span className="text-primary">{appointment.date}</span> at {appointment.slot}</p>
+                    <p>Please Pay: ${appointment.price}</p>
                 </div>
+            </div>
+            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div class="card-body">
+
+                    
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
+                  
+                </div>
+            </div>
+        </div>
     );
 };
 
